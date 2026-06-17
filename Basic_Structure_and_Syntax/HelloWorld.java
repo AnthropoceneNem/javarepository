edd586e4f487d0adece1d07e26c1335ca5f20b12
AnthropoceneNem <shantelleoreo@gmail.com>
2026-06-17 14:24:51 +0800

    df

--- Basic_Structure_and_Syntax/HelloWorld.java
+++ Basic_Structure_and_Syntax/HelloWorld.java
@@ -3,17 +3,17 @@ package Basic_Structure_and_Syntax;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        // Simple text lines (using \n at the end to force a new line)
+        
         System.out.print("Hello, World!\n");
         System.out.print("I'm a Java programmer!\n");
         System.out.print("I love coding in Java!\n");
         System.out.print("Java is a versatile programming language!\n");
         System.out.print(136533140258L + "\n"); 
 
-        // Math Operations using print
+       
         System.out.print("Addition: ");
         System.out.print(2 + 3); 
-        System.out.print("\n"); // Moves to the next line manually
+        System.out.print("\n");
 
         System.out.print("Subtraction: ");
         System.out.print(6 - 1);
