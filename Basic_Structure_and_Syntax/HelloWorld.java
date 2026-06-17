035abd1e6758a5c75226d6b2296fb3102668d0fc
AnthropoceneNem <shantelleoreo@gmail.com>
2026-06-17 13:02:26 +0800

    java1

---
+++ Basic_Structure_and_Syntax/HelloWorld.java
@@ -0,0 +1,13 @@
+package Basic_Structure_and_Syntax;
+// This is a simple Java program that prints "Hello, World!" to the console.
+
+public class HelloWorld {
+    //this is class declaration
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("I'm a Java programmer!");
+        System.out.println("I love coding in Java!");
+        System.out.println("Java is a versatile programming language!");
+    }
+}
