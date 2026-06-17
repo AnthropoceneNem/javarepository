4e56661604447c1cfdc931ce983e52502520b23b
AnthropoceneNem <shantelleoreo@gmail.com>
2026-06-17 14:18:17 +0800

    as

--- Basic_Structure_and_Syntax/HelloWorld.java
+++ Basic_Structure_and_Syntax/HelloWorld.java
@@ -10,8 +10,15 @@ public class HelloWorld {
         System.out.println("I love coding in Java!");
         System.out.println("Java is a versatile programming language!");
         System.out.println("136533140258l");
+        System.out.println("Addition");
+        System.out.println("2 + 3 = 5");
         System.out.println("Subtraction");
         System.out.println("6 - 1 = 5");
+        System.out.println("Multiplication");
+        System.out.println("4 * 5 = 20");
+        System.out.println("Division");
+        System.out.println("10 / 2 = 5");
+        
 
     }
 }
