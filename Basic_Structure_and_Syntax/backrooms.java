91a196896a8857f64af077f00274a9127fb0d4e7
AnthropoceneNem <shantelleoreo@gmail.com>
2026-06-18 14:42:34 +0800

    2ndday

---
+++ Basic_Structure_and_Syntax/backrooms.java
@@ -0,0 +1,28 @@
+package Basic_Structure_and_Syntax;
+
+public class backrooms {
+    
+    public static void main(String[] args) {
+        String MyFavoriteGame = "Backrooms";
+        System.out.println("My Favorite Game:" + MyFavoriteGame);
+        String Description = "The Backrooms is a popular survival horror game based on the internet creepypasta about \"noclipping\" out of reality. Players are trapped in a surreal, seemingly infinite labyrinth of liminal spaces—such as empty, yellow-wallpapered offices—where they must solve puzzles, manage sanity, and avoid terrifying monsters to escape.";
+        System.out.println("Description:" + Description);
+        float downloadSize = 3.5f;
+        System.out.println("Download Size: " + downloadSize + " GB");
+        char classDifficulty = '1';
+        System.out.println("Class Difficulty: " + classDifficulty);
+        boolean isFree = true;
+        System.out.println("Is the game free: " + isFree);
+        boolean isInstalled = true;
+        System.out.println("Is Installed: " + isInstalled);
+        System.out.print("Welcome to the Backrooms!\n");
+        System.out.print("You find yourself in a strange, endless maze of yellow-walled rooms.\n");
+        System.out.print("The air is musty, and the fluorescent lights flicker overhead.\n");
+        System.out.print("You must navigate through the maze and find a way out.\n");
+        System.out.print("Be careful, as there are dangers lurking in the shadows.\n");
+        System.out.print("Good luck!\n");
+
+
+    }
+    
+}
---
+++ Basic_Structure_and_Syntax/variable.java
@@ -0,0 +1,39 @@
+package Basic_Structure_and_Syntax;
+public class variable {
+
+public static void main(String[] args) {
+    String name = "Lindsay Shantelle Chua";
+    final int age = 17; 
+    int score = 10;
+
+    
+    System.out.println("Name:" + name );
+    System.out.println("Score:" + score );
+    score  = 16;
+
+    System.out.println("Updated age: " + age );
+    float height = 5.3f;
+    System.out.println("Height: " + height);
+    char section = 'P';
+    System.out.println("Section: " + section);
+    boolean regularStudent = true;
+    System.out.println("Regular Student:" + regularStudent);
+
+    String MyFavoriteGames = "Backrooms";
+    System.out.println("My Favorite Games:" + MyFavoriteGames);
+    String Description = "The Backrooms is a popular survival horror game based on the internet creepypasta about \"noclipping\" out of reality. Players are trapped in a surreal, seemingly infinite labyrinth of liminal spaces—such as empty, yellow-wallpapered offices—where they must solve puzzles, manage sanity, and avoid terrifying monsters to escape.";
+    System.out.println("Description:" + Description);
+    float downloadSize = 3.5f;
+    System.out.println("Download Size: " + downloadSize + " GB");
+    char classDifficulty = '1';
+    System.out.println("Class Difficulty: " + classDifficulty);
+    boolean isFree = true;
+    System.out.println("Is the game free: " + isFree);
+    
+
+
+
+
+    
+}
+}
